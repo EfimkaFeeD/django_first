8c986b8b05affdd6546fa5b1a710ac6c16cc598d
efimkafeed <efimkafeed@gmail.com>
2023-10-04 20:01:01 +0300

    Task 2 and 3

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
+![pipline](git@gitlab.crja72.ru:django_2023/students/144959-efimkafeed-47231.git/badges/main/pipeline.svg)
+# Создание и включение venv:
+```sh
 python -m venv venv
 venv/scripts/activate
-pip install -r requirements.txt
+```
+
+# Установка зависимостей:
+```sh
+pip install -r requirements/prod.txt
+```
+
+# Включение сервера:
+```sh
 cd lyceum
 python manage.py runserver
+```
--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "very_secret"
+load_dotenv(os.path.join(os.path.dirname(__file__), "test.env"))
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -94,15 +97,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".MinimumLengthValidator",
+        ".MinimumLengthValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".CommonPasswordValidator",
+        ".CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation"
-                ".NumericPasswordValidator",
+        ".NumericPasswordValidator",
     },
 ]
 
